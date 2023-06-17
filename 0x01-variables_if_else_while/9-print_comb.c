121d76d10c7a11d0d2e0a6a283b731c45bb97be4
thSelim <theselim98@gmail.com>
2023-06-17 05:01:07 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,23 +10,21 @@
 
 int main(void)
 {
-	int i, j;
 
-	for (i = 0; i < 9; i++)
+	int digit = 0;
+
+	while (digit <= 9)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(digit + 48);
+
+		if (digit != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-			if (!(i == 8 && j == 9))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
 		}
+		digit++;
 	}
-	putchar('\n');
+	putchar("\n";
+
 	return (0);
 }
