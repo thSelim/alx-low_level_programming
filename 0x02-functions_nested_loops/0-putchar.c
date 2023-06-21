ef84512836e52effc48d549aa3e91a51d52e22e9
thSelim <theselim98@gmail.com>
2023-06-21 04:58:59 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 	char str[] = "_putchar";
-		int ch
 
-		for (ch = 0; ch < 8; ch++)
-			_putchar(str[ch]);
+	int ch
+	for (ch = 0; ch < 8; ch++)
+	_putchar(str[ch]);
 	_putchar("\n");
 
 	return (0);
