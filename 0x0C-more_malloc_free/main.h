cf0cfa597899f947a280498e14458ea8fbaded9a
thSelim <theselim98@gmail.com>
2023-07-13 05:52:29 -0700

    a

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,12 @@
-#define main_h
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif
